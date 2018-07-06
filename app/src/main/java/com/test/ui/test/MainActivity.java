a7d715d4ab3acb34cbc0edab8b3076874abb5931
wenxiang <zhouwenxiang@km.com>
2018-07-06 12:05:13 +0800

    Read for maven build;

--- app/src/main/java/com/test/ui/test/MainActivity.java
+++ app/src/main/java/com/test/ui/test/MainActivity.java
@@ -3,11 +3,15 @@ package com.test.ui.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.winson.ui.widget.RateLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RateLayout rateLayout = new RateLayout(this);
     }
 }
