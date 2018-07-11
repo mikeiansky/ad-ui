3d782fbde78afae904ddd004cd3ac86e3f6be71b
wenxiang <zhouwenxiang@km.com>
2018-07-11 15:39:21 +0800

    update empty view utils;

--- ad-ui/src/main/java/com/winson/ui/widget/EmptyViewUtils.java
+++ ad-ui/src/main/java/com/winson/ui/widget/EmptyViewUtils.java
@@ -34,40 +34,42 @@ public class EmptyViewUtils {
     }
 
     public static View showLoadingView(ViewGroup parent, View customLoadingView) {
+        return showLoadingView(parent, customLoadingView, null);
+    }
+
+    public static View showLoadingView(ViewGroup parent, View customLoadingView, View.OnClickListener clickListener) {
         if (parent == null) {
             return null;
         }
-        removeEmptyView(parent);
-        removeErrorView(parent);
-        View loadingView = parent.findViewById(R.id.loading_view);
-        if (customLoadingView != loadingView) {
-            removeLoadingView(parent, false);
-            customLoadingView.setId(R.id.loading_view);
-            customLoadingView.setLayoutParams(getLayoutParams());
-            parent.addView(customLoadingView, 0);
-        }
+        removeAllView(parent);
+//        View loadingView = parent.findViewById(R.id.loading_view);
+//        if (customLoadingView != loadingView) {
+        customLoadingView.setId(R.id.loading_view);
+        customLoadingView.setLayoutParams(getLayoutParams());
+        customLoadingView.setOnClickListener(clickListener);
+        parent.addView(customLoadingView, 0);
+//        }
         show(parent, customLoadingView);
         return customLoadingView;
     }
 
     public static View showLoadingView(ViewGroup parent) {
-        return showLoadingView(parent, IGNORE_TEXT_SIZE, IGNORE_COLOR);
+        return showLoadingView(parent, 0, IGNORE_COLOR);
     }
 
     public static View showLoadingView(ViewGroup parent, int pbSize, int backgroundColor) {
         if (parent == null) {
             return null;
         }
-        removeEmptyView(parent);
-        removeErrorView(parent);
+        removeAllView(parent);
         View loadingView = parent.findViewById(R.id.loading_view);
-        if (loadingView == null) {
-            loadingView = LayoutInflater.from(parent.getContext()).inflate(R.layout.loading_view, parent, false);
-            loadingView.setId(R.id.loading_view);
-            loadingView.setLayoutParams(getLayoutParams());
-            parent.addView(loadingView, 0);
-        }
-        if (pbSize != IGNORE_TEXT_SIZE) {
+//        if (loadingView == null) {
+        loadingView = LayoutInflater.from(parent.getContext()).inflate(R.layout.loading_view, parent, false);
+        loadingView.setId(R.id.loading_view);
+        loadingView.setLayoutParams(getLayoutParams());
+        parent.addView(loadingView, 0);
+//        }
+        if (pbSize != 0) {
             ProgressBar pb = (ProgressBar) loadingView.findViewById(R.id.progress_bar);
             if (pb != null) {
                 ViewGroup.LayoutParams lp = pb.getLayoutParams();
@@ -87,19 +89,18 @@ public class EmptyViewUtils {
         if (parent == null) {
             return null;
         }
-        removeEmptyView(parent);
-        removeLoadingView(parent, false);
-        View errorView = parent.findViewById(R.id.error_view);
-        if (customErrorView != errorView) {
-            removeErrorView(parent);
-            customErrorView.setId(R.id.error_view);
-            customErrorView.setLayoutParams(getLayoutParams());
-            parent.addView(customErrorView, 0);
-        }
+        removeAllView(parent);
+//        View errorView = parent.findViewById(R.id.error_view);
+//        if (customErrorView != errorView) {
+        customErrorView.setId(R.id.error_view);
+        customErrorView.setLayoutParams(getLayoutParams());
+        customErrorView.setOnClickListener(clickListener);
+        parent.addView(customErrorView, 0);
+//        }
         return customErrorView;
     }
 
-    public static View showErrorView(ViewGroup parent, View customErrorView) {
+    public static View showErrorView(ViewGroup parent, View customErrorView, int tag) {
         return showErrorView(parent, customErrorView, null);
     }
 
@@ -107,15 +108,14 @@ public class EmptyViewUtils {
         if (parent == null) {
             return null;
         }
-        removeEmptyView(parent);
-        removeLoadingView(parent, false);
+        removeAllView(parent);
         View errorView = parent.findViewById(R.id.error_view);
-        if (errorView == null) {
-            errorView = LayoutInflater.from(parent.getContext()).inflate(R.layout.error_view, parent, false);
-            errorView.setLayoutParams(getLayoutParams());
-            errorView.setId(R.id.error_view);
-            parent.addView(errorView, 0);
-        }
+//        if (errorView == null) {
+        errorView = LayoutInflater.from(parent.getContext()).inflate(R.layout.error_view, parent, false);
+        errorView.setLayoutParams(getLayoutParams());
+        errorView.setId(R.id.error_view);
+        parent.addView(errorView, 0);
+//        }
         TextView msgText = (TextView) errorView.findViewById(R.id.msg);
         if (msg != null) {
             msgText.setText(msg);
@@ -154,19 +154,18 @@ public class EmptyViewUtils {
         if (parent == null) {
             return null;
         }
-        removeErrorView(parent);
-        removeLoadingView(parent, false);
-        View errorView = parent.findViewById(R.id.empty_view);
-        if (customEmptyView != errorView) {
-            removeEmptyView(parent);
-            customEmptyView.setId(R.id.empty_view);
-            customEmptyView.setLayoutParams(getLayoutParams());
-            parent.addView(customEmptyView, 0);
-        }
+        removeAllView(parent);
+//        View errorView = parent.findViewById(R.id.empty_view);
+//        if (customEmptyView != errorView) {
+        customEmptyView.setId(R.id.empty_view);
+        customEmptyView.setLayoutParams(getLayoutParams());
+        customEmptyView.setOnClickListener(clickListener);
+        parent.addView(customEmptyView, 0);
+//        }
         return customEmptyView;
     }
 
-    public static View showEmptyView(ViewGroup parent, View customEmptyView) {
+    public static View showEmptyView(ViewGroup parent, View customEmptyView, int tag) {
         return showEmptyView(parent, customEmptyView, null);
     }
 
@@ -174,8 +173,7 @@ public class EmptyViewUtils {
         if (parent == null) {
             return null;
         }
-        removeErrorView(parent);
-        removeLoadingView(parent, false);
+        removeAllView(parent);
         View emptyView = parent.findViewById(R.id.empty_view);
         if (emptyView == null) {
             emptyView = LayoutInflater.from(parent.getContext()).inflate(R.layout.empty_view, parent, false);
@@ -205,8 +203,8 @@ public class EmptyViewUtils {
         return showEmptyView(parent, null, IGNORE_COLOR, IGNORE_TEXT_SIZE, IGNORE_COLOR, listener);
     }
 
-    public static View showEmptyView(ViewGroup parent, int color, View.OnClickListener listener) {
-        return showEmptyView(parent, null, color, IGNORE_TEXT_SIZE, IGNORE_COLOR, listener);
+    public static View showEmptyView(ViewGroup parent, int backgroundColor, View.OnClickListener listener) {
+        return showEmptyView(parent, null, backgroundColor, IGNORE_TEXT_SIZE, IGNORE_COLOR, listener);
     }
 
     public static View showEmptyView(ViewGroup parent, String msg, View.OnClickListener listener) {
@@ -217,27 +215,27 @@ public class EmptyViewUtils {
         return showEmptyView(parent, null, IGNORE_COLOR, textSize, textColor, listener);
     }
 
-    public static void removeEmptyView(ViewGroup parent) {
-        if (parent == null) {
-            return;
-        }
-        parent.removeView(parent.findViewById(R.id.empty_view));
-    }
-
-    public static void removeErrorView(ViewGroup parent) {
-        if (parent == null) {
-            return;
-        }
-        parent.removeView(parent.findViewById(R.id.error_view));
-    }
-
-    public static void removeLoadingView(final ViewGroup parent, boolean animation) {
-        if (parent == null) {
-            return;
-        }
-        final View loadingView = parent.findViewById(R.id.loading_view);
-        parent.removeView(loadingView);
-    }
+//    public static void removeEmptyView(ViewGroup parent) {
+//        if (parent == null) {
+//            return;
+//        }
+//        parent.removeView(parent.findViewById(R.id.empty_view));
+//    }
+//
+//    public static void removeErrorView(ViewGroup parent) {
+//        if (parent == null) {
+//            return;
+//        }
+//        parent.removeView(parent.findViewById(R.id.error_view));
+//    }
+//
+//    public static void removeLoadingView(final ViewGroup parent, boolean animation) {
+//        if (parent == null) {
+//            return;
+//        }
+//        final View loadingView = parent.findViewById(R.id.loading_view);
+//        parent.removeView(loadingView);
+//    }
 
     public static void removeAllView(ViewGroup parent) {
         if (parent == null) {
--- app/src/main/java/com/test/ui/test/MainActivity.java
+++ app/src/main/java/com/test/ui/test/MainActivity.java
@@ -2,16 +2,58 @@ package com.test.ui.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.winson.ui.widget.EmptyViewUtils;
 import com.winson.ui.widget.RateLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ViewGroup emptyGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RateLayout rateLayout = new RateLayout(this);
+        emptyGroup = findViewById(R.id.empty_panel);
+
+        findViewById(R.id.load).setOnClickListener(this);
+        findViewById(R.id.error).setOnClickListener(this);
+        findViewById(R.id.empty).setOnClickListener(this);
+        findViewById(R.id.load_c).setOnClickListener(this);
+        findViewById(R.id.error_c).setOnClickListener(this);
+        findViewById(R.id.empty_c).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.load:
+                EmptyViewUtils.showLoadingView(emptyGroup);
+                break;
+            case R.id.error:
+                EmptyViewUtils.showErrorView(emptyGroup, null);
+                break;
+            case R.id.empty:
+                EmptyViewUtils.showEmptyView(emptyGroup, null);
+                break;
+            case R.id.load_c:
+                View load_c = LayoutInflater.from(this).inflate(R.layout.load_c, emptyGroup, false);
+                EmptyViewUtils.showLoadingView(emptyGroup, load_c);
+                break;
+            case R.id.error_c:
+                View error_c = LayoutInflater.from(this).inflate(R.layout.error_c, emptyGroup, false);
+                EmptyViewUtils.showLoadingView(emptyGroup, error_c);
+                break;
+            case R.id.empty_c:
+                View empty_c = LayoutInflater.from(this).inflate(R.layout.empty_c, emptyGroup, false);
+                EmptyViewUtils.showLoadingView(emptyGroup, empty_c);
+                break;
+        }
     }
 }
