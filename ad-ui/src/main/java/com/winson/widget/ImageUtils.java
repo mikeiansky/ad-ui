f4a32e5c33b11107048af0dbbeae1838c20f03ac
winson <929566854@qq.com>
2018-09-12 17:25:36 +0800

    complete image utils;

--- ad-ui/src/main/java/com/winson/widget/ImageUtils.java
+++ ad-ui/src/main/java/com/winson/widget/ImageUtils.java
@@ -3,41 +3,129 @@ package com.winson.widget;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.Shader;
+import android.support.annotation.NonNull;
+import android.widget.ImageView;
 
 public class ImageUtils {
 
-    public static Bitmap convertRoundBitmap(Context context, int resId, float radius) {
+    public static Bitmap convertRoundBitmap(Context context, int resId, float radius, ImageView.ScaleType scaleType) {
         Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId);
         return convertRoundBitmap(bitmap, radius);
     }
 
+    public static Bitmap convertRoundBitmap(Context context, int resId, int targetWidth, int targetHeight, float radius, ImageView.ScaleType scaleType) {
+        if (targetWidth == 0 || targetHeight == 0) {
+            return null;
+        }
+        BitmapFactory.Options options = new BitmapFactory.Options();
+//        options.inJustDecodeBounds = true;
+//
+//        BitmapFactory.decodeResource(context.getResources(), resId, options);
+//        int width = options.outWidth;
+//        int height = options.outHeight;
+//
+//        if (width == 0 || height == 0) {
+//            return null;
+//        }
+//
+//        int cws = width / targetWidth;
+//        int chs = height / targetHeight;
+//
+//        int sampleSize = cws > chs ? cws : chs;
+////        sampleSize = sampleSize == 0 ? 1 : sampleSize;
+////        int sampleSize = 1;
+//
+//        options.inSampleSize = sampleSize;
+//        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId, options);
+        return getRoundBitmap(bitmap, targetWidth, targetHeight, radius, scaleType);
+    }
+
     public static Bitmap convertRoundBitmap(Bitmap bitmap, float radius) {
+        return convertRoundBitmap(bitmap, radius, ImageView.ScaleType.CENTER_INSIDE);
+    }
+
+    public static Bitmap convertRoundBitmap(Bitmap bitmap, float radius, ImageView.ScaleType scaleType) {
         if (bitmap == null) {
             return null;
         }
+        return getRoundBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), radius, ImageView.ScaleType.CENTER_INSIDE);
+    }
 
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
+    @NonNull
+    private static Bitmap getRoundBitmap(Bitmap bitmap, int targetWidth, int targetHeight, float radius, ImageView.ScaleType scaleType) {
+        int width;
+        int height;
+        width = bitmap.getWidth();
+        height = bitmap.getHeight();
 
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        float sw = targetWidth * 1f / width;
+        float sh = targetHeight * 1f / height;
+
+        Matrix matrix = new Matrix();
+        RectF rectF = new RectF();
+
+        switch (scaleType) {
+            case CENTER_CROP:
+                float ss = sw < sh ? sh : sw;
+
+                float rw = width * ss;
+                float rh = height * ss;
+                float tx = targetWidth / 2f - rw / 2f;
+                float ty = targetHeight / 2f - rh / 2f;
+
+                matrix.preTranslate(tx, ty);
+                matrix.preScale(ss, ss);
+
+                rectF.left = 0;
+                rectF.top = 0;
+                rectF.right = targetWidth;
+                rectF.bottom = targetHeight;
+
+                break;
+            case FIT_XY:
+                matrix.preScale(sw, sh);
+
+                rectF.left = 0;
+                rectF.top = 0;
+                rectF.right = targetWidth;
+                rectF.bottom = targetHeight;
+                break;
+            default:
+                ss = sw > sh ? sh : sw;
+
+                rw = width * ss;
+                rh = height * ss;
+
+                tx = targetWidth / 2f - rw / 2f;
+                ty = targetHeight / 2f - rh / 2f;
+
+                matrix.preTranslate(tx, ty);
+                matrix.preScale(ss, ss);
+
+                rectF.left = tx;
+                rectF.top = ty;
+                rectF.right = tx + rw;
+                rectF.bottom = ty + rh;
+                break;
+        }
+
+        Bitmap output = Bitmap.createBitmap(targetWidth, targetHeight, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-        RectF rectF = new RectF(0, 0, width, height);
         canvas.drawRoundRect(rectF, radius, radius, paint);
 
         PorterDuffXfermode mode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
         paint.setXfermode(mode);
 
-        canvas.drawBitmap(bitmap, new Rect(0, 0, (int)rectF.width(), (int)rectF.height()), rectF, paint);
+        canvas.drawBitmap(bitmap, matrix, paint);
 
         bitmap.recycle();
 
